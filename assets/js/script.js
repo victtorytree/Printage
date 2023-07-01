b8687d00ea91ed82af2324e3a152ac2b04598d02
vica <vicarom806@gmail.com>
2023-07-01 10:27:30 +0300

    fixed first screen arrow behaviour, javascript syntax corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-window.addEventListener('scroll', function() {
-    const array = document.querySelectorAll('.project');
-    array.forEach(element => {
-    if(element.getBoundingClientRect().top < 300) {
-        element.classList.add('active');
-        element.querySelector('.video_').play();
-        element.querySelector('.video_small').play();
-    } else {
-        element.classList.remove('active');
-        element.querySelector('.video_').pause();
-        element.querySelector('.video_small').pause();
-    }
+window.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        const array = document.querySelectorAll('.project');
+        array.forEach(element => {
+        if(element.getBoundingClientRect().top < 300) {
+            element.classList.add('active');
+            element.querySelector('.video_').play();
+            element.querySelector('.video_small').play();
+        } else {
+            element.classList.remove('active');
+            element.querySelector('.video_').pause();
+            element.querySelector('.video_small').pause();
+        }
+        });
     });
-    
+
 });
+
+
+
